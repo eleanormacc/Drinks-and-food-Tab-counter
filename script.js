e1f0254e84caa39727c854174b3ec9de98d6f647
Eleanor MacCarthy <115783158+eleanormacc@users.noreply.github.com>
2024-03-21 09:59:17 +0000

    Update script.js
    add date and final total amount works

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ function addInvoiceItem(){
     <td><input type="text" class="form-control" placeholder="Enter Item Name" required></td>
     <td><input type="number" class="form-control itemPrice" placeholder="Enter Item Price" required></td>
     <td><input type="number" class="form-control quantity" placeholder="Enter Quantity" required></td>
+    <td><input type="text" class="form-control totalItemPrice" disabled readonly></td>
     <td><button type="button class="btn btn-danger" onclick="removeInvoiceItem(${itemCounter})">Remove</button></td>
     `;
 
@@ -24,5 +25,30 @@ function removeInvoiceItem(itemId) {
         updateTotalAmount();
 }
 
-function updateTotalAmount(){
+//Final Total Amount
+function updateTotalAmount() {
+    let finalTotalAmount =0; //new variable declared 
+
+    $("tr[id^='itemRow']").each(function(){
+    const itemPrice = parseFloat($(this).find(".itemPrice").val()) || 0;
+    const quantity = parseFloat($(this).find(".quantity").val()) || 0;
+    const totalItemPrice  = itemPrice * quantity;
+
+    $(this).find(".totalItemPrice").val(totalItemPrice.toFixed(2));
+    finalTotalAmount += totalItemPrice;
+    });
+
+    $("#finalTotalAmount").val(finalTotalAmount.toFixed(2));
 }
+
+//Invoice Date: automatically generated 
+$(document).ready(function () {
+    const currentDate = new Date();
+    const formattedDate = currentDate.toISOString().slice(0, 10); 
+    $("#invoiceDate").val(formattedDate);
+    });
+
+    ("#invoiceForm").submit(function (event){
+    event.preventDefault();
+    });
+    updateTotalAmount();
