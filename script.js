ed8310bc914ade36a46bbb9bb1185262802fd6d0
Eleanor MacCarthy <115783158+eleanormacc@users.noreply.github.com>
2024-03-21 05:55:02 +0000

    Update script.js
    Adding changes to code to be able to add quantity

--- script.js
+++ script.js
@@ -1,99 +1,28 @@
-$(document).ready(function (){
+let itemCounter = 0;
 
-    var items = [];
-//All Buttons
-    $("#item-form").on("submit, addItemToCart");
-    $("#cart-table").on("click", ".btn-danger", removeItemFromCart);
-    $("#generate-invoice").on("click, generateInvoice");
+//Add Invoice Items
+function addInvoiceItem(){
+    itemCounter++;
 
-//Action in relation to adding items to cart 
-    function addItemToCart(event){
-        event.preventDefault();
+    const newItemRow = `
+    <tr id="itemRow${itemCounter}">
+    <td><input type="text" class="form-control" placeholder="Enter Item Name" required></td>
+    <td><input type="number" class="form-control itemPrice" placeholder="Enter Item Price" required></td>
+    <td><input type="number" class="form-control quantity" placeholder="Enter Quantity" required></td>
+    <td><button type="button class="btn btn-danger" onclick="removeInvoiceItem(${itemCounter})">Remove</button></td>
+    `;
 
-        //Step 1. Declare variables
-        var itemName = $("#item-name").val();
-        var itemPrice = $("#item-price").val();
+    $("#invoiceItems").append(newItemRow);
 
-        //Step 2. Function 
-        if(itemName !== "" && itemPrice !== ""){
-            var item = {
-                name: itemName,
-                price: parseFloat(itemPrice),
-            };
+    //update total amount on every item added
+    updateTotalAmount();
+}
 
-            //action to add item to the cart 
-            items.push(item);
-            $("#cart-table tbody").append(
-                "<tr><td>" + 
-                item.name + 
-                "</td><td>$" + 
-                item.price.toFixed(2) + 
-                '</td><td><button class="btn btn-sm btn-danger"><i class="fa fa-trash-alt"></i></button></td></tr>'
-            );
-            
-            //Total cost of items changes when buttons is pressed and request is submitted
-            updateTotalCost();
-            $("#item-name").val("");
-            $("#item-price").val("");
-        }
-    }
+//Remove Invoice Items
+function removeInvoiceItem(itemId) {
+    $(`#itemRow${itemId}`).remove();
+        updateTotalAmount();
+}
 
-//Action in relation to removing items to cart 
-    function removeItemFromCart() {
-        var index = $(this).closest("tr").index();
-        items.slice(index, 1);
-        $(this).closest("tr").remove();
-        updateTotalCost();
-    }
-
-    function updateTotalCost() {
-        var totalCost = 0;
-        item.forEach(function (item){totalCost += item.price;});
-        $("total-cost").text("Total Cost: £" + totalCost.
-        toFixed
-        (2));
-    }
-
-// Generating an Invoice 
-    function generateInvoice(){
-      var invoice = `<html>
-      <head>
-          <title>Invoice</title>
-      </head>
-      <body>
-         <div class="container mt-5">
-          <h1 class="text-center">Invoice</h1>
-          <table class="table">
-              <thead>
-                  <tr>
-                      <th>Item Name</th>
-                      <th>Item Price</th>
-                      <th>Quantity</th>
-                  </tr>
-              </thead>
-              <tbody>`;
-            
-        items.forEach(function (item){
-        invoice += "<tr><td>" + item.name +"</td><td>$" +
-        item.price.toFixed(2)+"</td></tr>";
-        });
-
-        invoice += '</tbody></table><p class="text-right">Total Cost: £' + 
-        getTotalCost()+ 
-        "</p><div><body></html>";
-        
-        //creation of pop up to new window when invoice is generated 
-        var popup = window.open("", "_blank");
-        popup.document.open();
-        popup.document.write(invoice);
-        popup.document.close();
-    }
-
-    function getTotalCost(){
-        var totalCost = 0;
-        items.forEach(function (item){
-           totalCost += item.price;
-        })
-        return totalCost.toFixed(2);
-    }
-});
+function updateTotalAmount(){
+}
